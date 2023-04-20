46f48c9d0aceab8625875ce3859b66035d69a9b1
Jonathan.Donath.Lopez <Jonathan.Donath.Lopez@hotmail.com>
2023-04-20 21:54:00 +0200

    fixed formatting for bearer desc

--- advertisementAPI/Program.cs
+++ advertisementAPI/Program.cs
@@ -31,9 +31,9 @@ builder.Services.AddSwaggerGen(sw =>
     // Needed for authorisation ========================================================
     sw.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Description = $"JWT Authorization header using the Bearer scheme.\r\n\r\n" +
+                      $" Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+                      $" Example: 'Bearer 12345abcdef'",
         Name = "Authorization",
         In = ParameterLocation.Header,
         Type = SecuritySchemeType.ApiKey,
